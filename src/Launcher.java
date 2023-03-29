9da6e5cb8b9856f0fbc092ef08c859102482ce44
joseph.solier <joseph.solier@epita.fr>
2023-03-29 17:38:41 +0200

    part5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,43 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Launcher {
+
+    public static void print3words(String path)
+    {
+        String inputString = path;
+        Stream<String> wordsStream = Arrays.stream(inputString.split(" "))
+                .map(String::toLowerCase);
+        //wordsStream.forEach(System.out::println);
+        List<String> list = Arrays.asList("Java", "Python", "Java", "JavaScript", "C++", "Python");
+
+        Map<String, Long> counts = wordsStream
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<String, Long> sortedMap = new LinkedHashMap<>();
+        counts.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .forEachOrdered(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
+
+        int count = 0;
+        for (Map.Entry<String, Long> entry : sortedMap.entrySet()) {
+            if (count == 2) {
+                System.out.println(entry.getKey());
+                break;
+            }
+            System.out.print(entry.getKey() + " ");
+            count++;
+
+        }
+
+
+
+    }
     public static int fibonacci(int n) {
         if (n == 0) {
             return 0;
@@ -24,12 +61,27 @@ public class Launcher {
                 if (res.equals("fibo")) {
                     int n = scanner.nextInt();
                     System.out.println(fibonacci(n));
-                    scanner.nextLine();
+
                 } else {
-                    System.out.println("Unknown command");
+                    if (res.equals("freq")) {
+                        res = scanner.next();
+                        Path chemin = Paths.get(res);
+                        try{
+                            String contenu= Files.readString(chemin);
+
+                            //System.out.println(contenu);
+                            print3words(contenu);
+
+                            scanner.nextLine();
+                        }catch(IOException e) {
+                            System.out.println("Unreadable file :" + chemin);
+                            scanner.nextLine();
+                        }
+                    } else {
+                        System.out.println("Unknown command");
+                    }
                 }
             }
         }
     }
 }
-
