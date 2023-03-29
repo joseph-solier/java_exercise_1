cc77a26a10cf1a1efb381fc976bc45a7b757a1a1
joseph.solier <joseph.solier@epita.fr>
2023-03-29 15:21:45 +0200

    part4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome Back !");
 
@@ -10,9 +20,15 @@ public class Launcher {
             String res = scanner.nextLine();
             if (res.equals("quit")) {
                 return;
+            } else {
+                if (res.equals("fibo")) {
+                    int n = scanner.nextInt();
+                    System.out.println(fibonacci(n));
+                    scanner.nextLine();
+                } else {
+                    System.out.println("Unknown command");
+                }
             }
-
-            System.out.println("Unknown command");
         }
     }
 }
