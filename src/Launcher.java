8ed69cc003ee7da5e6bb54fb835012994cc61340
joseph.solier <joseph.solier@epita.fr>
2023-03-29 15:06:19 +0200

    part2

---
+++ src/Launcher.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Welcome Back !");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String res = scanner.nextLine();
+            if (res.equals("quit")) {
+                return;
+            } else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
+}
+
